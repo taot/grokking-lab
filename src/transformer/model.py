a4ef259b589b973301d813a43360024d9f89c53b
taot <librakevin@gmail.com>
2026-01-28 21:45:00 -0500

    add position encoding and layer norm to transformer

--- src/transformer/model.py
+++ src/transformer/model.py
@@ -25,16 +25,47 @@ class Attention:
         self.W_k = torch.Tensor(d, d_k)
         self.W_v = torch.Tensor(d, d_v)
 
+        # projection
+        self.W_proj = torch.Tensor(d_v, d)
+        self.b_proj = torch.Tensor(d)
+
     def forward(self, x: torch.Tensor, mask: Optional[torch.Tensor] = None) -> torch.Tensor:
         batch_size, seq_len = x.shape[:2]
         assert_dimension(x, (batch_size, seq_len, self.d))
 
         q = torch.matmul(x, self.W_q)       # batch_size * seq_len * d_k
-        k = torch.matmul(x, self.W_k)    # batch_size * seq_len * d_k
-        v = torch.matmul(x, self.W_v)    # batch_size * seq_len * d_v
+        k = torch.matmul(x, self.W_k)       # batch_size * seq_len * d_k
+        v = torch.matmul(x, self.W_v)       # batch_size * seq_len * d_v
 
         weights = torch.matmul(q, k.transpose(-1, -2)) / math.sqrt(self.d_k)   # batch_size * seq_len * seq_len
         weights_softmax = softmax(weights)          # batch_size * seq_len * seq_len
         attn = torch.matmul(weights_softmax, v)     # batch_size * seq_len * d_v
 
-        return attn
+        out = torch.matmul(attn, self.W_proj) + self.b_proj
+
+        return out
+
+
+def positional_encoding(seq_len: int, d: int, N: int = 100000) -> torch.Tensor:
+    pe = torch.zeros(seq_len, d)
+    for p in range(seq_len):
+        pe[p, ::2] = torch.sin(p / (N ** (torch.Tensor(range(d // 2)) * 2 / d)))
+        pe[p, 1::2] = torch.cos(p / (N ** (torch.Tensor(range(d // 2)) * 2 / d)))
+    return pe
+
+
+class LayerNorm:
+
+    def __init__(self, *, d: int, eps: float = 1e-8) -> None:
+        self.d = d
+        self.eps = eps
+        self.gamma = torch.ones(d) /d
+        self.beta = torch.zeros(d)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, correction=0, keepdim=True)
+        std = torch.sqrt(var + self.eps)
+
+        out = torch.mul((x - mean) / std, self.gamma) + self.beta
+        return out
--- tests/unit/transformer/test_model.py
+++ tests/unit/transformer/test_model.py
@@ -1,8 +1,10 @@
+import math
+
 import torch
 from torch import nn
 from torch.testing import assert_close
 
-from transformer.model import softmax, Attention
+from transformer.model import softmax, Attention, positional_encoding, LayerNorm
 
 
 def test_softmax() -> None:
@@ -12,31 +14,75 @@ def test_softmax() -> None:
     assert_close(y, torch.tensor([[0.268941, 0.098938], [1.987223, 0.731059]]))
 
 
-def test_attention() -> None:
-    d = 4
-    batch = 1
-    seq_len = 3
+# def test_attention() -> None:
+#     d = 4
+#     batch = 1
+#     seq_len = 3
+#
+#     my_mha = Attention(d, d, d)
+#     official_mha = nn.MultiheadAttention(d, 1, batch_first=True)
+#
+#     with torch.no_grad():
+#         # 注意 PyTorch MHA 的权重排列方式
+#         # my_mha.W_q.weight.copy_(official_mha.in_proj_weight[:d])
+#         # my_mha.W_k.weight.copy_(official_mha.in_proj_weight[d:2 * d])
+#         # my_mha.W_v.weight.copy_(official_mha.in_proj_weight[2 * d:])
+#         # my_mha.W_o.weight.copy_(official_mha.out_proj.weight)
+#
+#         W = official_mha.in_proj_weight.detach()
+#         my_mha.W_q = W[:d].clone()
+#         my_mha.W_k = W[d:2 * d].clone()
+#         my_mha.W_v = W[2 * d:].clone()
+#
+#         official_mha.out_proj.weight
+#
+#     # 测试
+#     x = torch.randn(batch, seq_len, d)
+#
+#     official_out, official_attn = official_mha(x, x, x)
+#     my_out = my_mha.forward(x, None)
+#
+#     print(f"输出差异: {(official_out - my_out).abs().max():.2e}")  # 应该 < 1e-5
+#     # print(f"Attention 差异: {(official_attn - my_attn).abs().max():.2e}")
+#     assert_close(my_out, official_out)
 
-    my_mha = Attention(d, d, d)
-    official_mha = nn.MultiheadAttention(d, 1, batch_first=True)
 
-    with torch.no_grad():
-        # 注意 PyTorch MHA 的权重排列方式
-        # my_mha.W_q.weight.copy_(official_mha.in_proj_weight[:d])
-        # my_mha.W_k.weight.copy_(official_mha.in_proj_weight[d:2 * d])
-        # my_mha.W_v.weight.copy_(official_mha.in_proj_weight[2 * d:])
-        # my_mha.W_o.weight.copy_(official_mha.out_proj.weight)
+def plain_positional_encoding(p: int, j: int, d: int, N: int) -> float:
+    if j % 2 == 0:
+        i = j // 2
+        v = math.sin(p / math.pow(N, 2 * i / d))
+    else:
+        i = (j - 1) // 2
+        v = math.cos(p / math.pow(N, 2 * i / d))
+    return v
 
-        W = official_mha.in_proj_weight.detach()
-        my_mha.W_q = W[:d].clone()
-        my_mha.W_k = W[d:2 * d].clone()
-        my_mha.W_v = W[2 * d:].clone()
 
-    # 测试
-    x = torch.randn(batch, seq_len, d)
+def test_positional_encoding() -> None:
+    pe = positional_encoding(seq_len=5, d=4, N=8)
 
-    official_out, official_attn = official_mha(x, x, x)
-    my_out = my_mha.forward(x, None)
+    expected = torch.Tensor([
+            [ 0.000000,  1.000000,  0.000000,  1.000000],
+            [ 0.841471,  0.540302,  0.346234,  0.938148],
+            [ 0.909297, -0.416147,  0.649637,  0.760245],
+            [ 0.141120, -0.989992,  0.872678,  0.488296],
+            [-0.756802, -0.653644,  0.987766,  0.155944]
+    ])
 
-    print(f"输出差异: {(official_out - my_out).abs().max():.2e}")  # 应该 < 1e-5
-    # print(f"Attention 差异: {(official_attn - my_attn).abs().max():.2e}")
+    assert_close(pe, expected)
+
+
+def test_layer_norm() -> None:
+    layer_norm = LayerNorm(d=3)
+    layer_norm.beta = torch.Tensor([0.1, 0.2, 0.3])
+
+    x = torch.Tensor([
+        [1, 2, 3],
+        [1, 4, 9],
+    ])
+    out = layer_norm.forward(x)
+
+    expected = torch.Tensor([
+        [-0.308248, 0.200000, 0.708248],
+        [-0.270389, 0.132657, 0.737733]
+    ])
+    assert_close(out, expected)
