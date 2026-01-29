fix softmax

src/transformer/model.py
@@ -10,24 +10,38 @@ def assert_dimension(x: torch.Tensor, dim: tuple[int, ...]) -> None:
 
 def softmax(x: torch.Tensor) -> torch.Tensor:
     exp_x = torch.exp(x)
-    sum_x = torch.sum(exp_x, dim=-1)
+    sum_x = torch.sum(exp_x, dim=-1, keepdim=True)
     return exp_x / sum_x
 
 
+def relu(x: torch.Tensor) -> torch.Tensor:
+    x[x < 0] = 0
+    return x
+
+
+def xavier_init(fan_in: int, fan_out: int):
+    std = math.sqrt(2.0 / (fan_in + fan_out))
+    return torch.randn(fan_out, fan_in) * std
+
+
 class Attention:
+
+    # TODO Multi-head
+    # TODO Causal mask
+    # TODO Test
 
     def __init__(self, d: int, d_k: int, d_v: int) -> None:
         self.d = d
         self.d_k = d_k
         self.d_v = d_v
 
-        self.W_q = torch.Tensor(d, d_k)
-        self.W_k = torch.Tensor(d, d_k)
-        self.W_v = torch.Tensor(d, d_v)
+        self.W_q = xavier_init(d, d_k)
+        self.W_k = xavier_init(d, d_k)
+        self.W_v = xavier_init(d, d_v)
 
         # projection
-        self.W_proj = torch.Tensor(d_v, d)
-        self.b_proj = torch.Tensor(d)
+        self.W_proj = xavier_init(d_v, d)
+        self.b_proj = torch.zeros(d)
 
     def forward(self, x: torch.Tensor, mask: Optional[torch.Tensor] = None) -> torch.Tensor:
         batch_size, seq_len = x.shape[:2]
@@ -46,9 +60,9 @@ class Attention:
         return out
 
 
-def positional_encoding(seq_len: int, d: int, N: int = 100000) -> torch.Tensor:
-    pe = torch.zeros(seq_len, d)
-    for p in range(seq_len):
+def positional_encoding(max_seq_len: int, d: int, N: int = 100000) -> torch.Tensor:
+    pe = torch.zeros(max_seq_len, d)
+    for p in range(max_seq_len):
         pe[p, ::2] = torch.sin(p / (N ** (torch.Tensor(range(d // 2)) * 2 / d)))
         pe[p, 1::2] = torch.cos(p / (N ** (torch.Tensor(range(d // 2)) * 2 / d)))
     return pe
@@ -69,3 +83,68 @@ class LayerNorm:
 
         out = torch.mul((x - mean) / std, self.gamma) + self.beta
         return out
+
+
+class Embedding:
+
+    def __init__(self, *, vocab_size: int, d: int) -> None:
+        self.vocab_size = vocab_size
+        self.d = d
+        self.weights = torch.randn(vocab_size, d) / math.sqrt(d)
+
+    def forward(self, input_ids: torch.Tensor) -> torch.Tensor:
+        emb = self.weights[input_ids]
+        return emb
+
+
+class DecoderBlock:
+    def __init__(self, d: int) -> None:
+
+        self.layer_norm1 = LayerNorm(d=d)
+        self.attention = Attention(d=d, d_k=d, d_v=d)
+
+        # TODO residual connection
+
+        self.layer_norm2 = LayerNorm(d=d)
+        self.linear = xavier_init(d, d)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.layer_norm1.forward(x)
+        x = self.attention.forward(x, mask=None)
+        x = self.layer_norm2.forward(x)
+        x = torch.matmul(x, self.linear)
+        x = relu(x)
+
+        return x
+
+
+class Transformer:
+    def __init__(self, *, vocab_size: int, d: int, n_layers: int) -> None:
+        self.vocab_size = vocab_size
+        self.d = d
+
+        self.embedding = Embedding(vocab_size=vocab_size, d=d)
+        self.pe = positional_encoding(max_seq_len=100, d=d, N=10)
+        self.decoder_blocks = [DecoderBlock(d=d) for _ in range(n_layers)]
+        self.linear = xavier_init(d, d)
+
+    def forward(self, input_ids: torch.Tensor) -> torch.Tensor:
+        seq_len = input_ids.shape[-1]
+
+        x1 = self.embedding.forward(input_ids)
+        pe = self.pe[:seq_len, :].clone()
+        x2 = x1 + pe
+
+        # for decoder_block in self.decoder_blocks:
+        assert len(self.decoder_blocks) == 1
+        x3 = self.decoder_blocks[0].forward(x2)
+
+        x4 = torch.matmul(x3, self.linear)
+        x5 = softmax(x4)
+
+        return x5
+
+
+# TODO loss function
+# TODO check backprop
+# TODO optimize

tests/unit/transformer/test_model.py
@@ -1,50 +1,66 @@
 import math
 
+import pytest
 import torch
 from torch import nn
 from torch.testing import assert_close
 
-from transformer.model import softmax, Attention, positional_encoding, LayerNorm
+from transformer.model import softmax, Attention, positional_encoding, LayerNorm, Embedding, relu, Transformer, xavier_init
 
 
 def test_softmax() -> None:
-    x = torch.Tensor([[1, 2], [3, 4]])
+    x = torch.Tensor([[1, 2], [3, 5]])
     y = softmax(x)
+    print("\ny:")
     print(y)
-    assert_close(y, torch.tensor([[0.268941, 0.098938], [1.987223, 0.731059]]))
+    expected = torch.Tensor([
+        [0.268941, 0.731059],
+        [0.119203, 0.880797]
+    ])
+    assert_close(y, expected)
 
 
-# def test_attention() -> None:
-#     d = 4
-#     batch = 1
-#     seq_len = 3
-#
-#     my_mha = Attention(d, d, d)
-#     official_mha = nn.MultiheadAttention(d, 1, batch_first=True)
-#
-#     with torch.no_grad():
-#         # 注意 PyTorch MHA 的权重排列方式
-#         # my_mha.W_q.weight.copy_(official_mha.in_proj_weight[:d])
-#         # my_mha.W_k.weight.copy_(official_mha.in_proj_weight[d:2 * d])
-#         # my_mha.W_v.weight.copy_(official_mha.in_proj_weight[2 * d:])
-#         # my_mha.W_o.weight.copy_(official_mha.out_proj.weight)
-#
-#         W = official_mha.in_proj_weight.detach()
-#         my_mha.W_q = W[:d].clone()
-#         my_mha.W_k = W[d:2 * d].clone()
-#         my_mha.W_v = W[2 * d:].clone()
-#
-#         official_mha.out_proj.weight
-#
-#     # 测试
-#     x = torch.randn(batch, seq_len, d)
-#
-#     official_out, official_attn = official_mha(x, x, x)
-#     my_out = my_mha.forward(x, None)
-#
-#     print(f"输出差异: {(official_out - my_out).abs().max():.2e}")  # 应该 < 1e-5
-#     # print(f"Attention 差异: {(official_attn - my_attn).abs().max():.2e}")
-#     assert_close(my_out, official_out)
+def test_softmax_batch() -> None:
+    x = torch.Tensor([
+        [
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+        ],
+        [
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+        ]
+    ])
+
+    y = softmax(x)
+
+    expected = torch.Tensor([
+        [
+            [0.250000, 0.250000, 0.250000, 0.250000],
+            [0.250000, 0.250000, 0.250000, 0.250000],
+            [0.250000, 0.250000, 0.250000, 0.250000]
+        ],
+        [
+            [0.250000, 0.250000, 0.250000, 0.250000],
+            [0.250000, 0.250000, 0.250000, 0.250000],
+            [0.250000, 0.250000, 0.250000, 0.250000]
+        ]
+    ])
+
+    assert_close(y, expected)
+
+
+def test_relu() -> None:
+    x = torch.Tensor([[1, -1, -2], [2, 0, 1]])
+    y = relu(x)
+    expected = torch.Tensor([
+        [1., 0., 0.],
+        [2., 0., 1.]
+    ])
+
+    assert_close(y, expected)
 
 
 def plain_positional_encoding(p: int, j: int, d: int, N: int) -> float:
@@ -58,7 +74,7 @@ def plain_positional_encoding(p: int, j: int, d: int, N: int) -> float:
 
 
 def test_positional_encoding() -> None:
-    pe = positional_encoding(seq_len=5, d=4, N=8)
+    pe = positional_encoding(max_seq_len=5, d=4, N=8)
 
     expected = torch.Tensor([
             [ 0.000000,  1.000000,  0.000000,  1.000000],
@@ -86,3 +102,58 @@ def test_layer_norm() -> None:
         [-0.270389, 0.132657, 0.737733]
     ])
     assert_close(out, expected)
+
+
+@pytest.mark.parametrize(
+    "input_ids, expected",
+    [
+        [
+            torch.LongTensor([2, 0, 1]),
+            torch.Tensor([
+                [5., 6.],
+                [1., 2.],
+                [3., 4.]
+            ])
+        ],
+        [
+            torch.LongTensor([
+                [0, 1, 2],
+                [1, 2, 0],
+            ]),
+            torch.Tensor([
+                [
+                    [1., 2.],
+                    [3., 4.],
+                    [5., 6.]
+                ],
+                [
+                    [3., 4.],
+                    [5., 6.],
+                    [1., 2.]
+                ]
+            ])
+        ]
+    ]
+)
+def test_embedding(input_ids: torch.Tensor, expected: torch.Tensor) -> None:
+    embedding = Embedding(vocab_size=3, d=2)
+    embedding.weights = torch.Tensor([
+        [1, 2],
+        [3, 4],
+        [5, 6],
+    ])
+
+    emb = embedding.forward(input_ids)
+    assert_close(emb, expected)
+
+
+def test_transformer() -> None:
+    transformer = Transformer(vocab_size=3, d=4, n_layers=1)
+
+    x = torch.LongTensor([
+        [2, 0, 1]
+    ])
+
+    out = transformer.forward(x)
+
+    print(out)